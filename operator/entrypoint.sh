85b3971f80c76a08c15f047d8f4c7410fd8c2c0c
dappnodedev <diego@dappnode.io>
2024-01-24 10:27:18 +0100

    Migrate to Holesky

--- operator/entrypoint.sh
+++ operator/entrypoint.sh
@@ -2,45 +2,48 @@
 
 # TODO: Allow adding more than 1 keystore (upload compressed file and extract inside keystore dir)
 
+# NETWORK to upper case
+UPPER_CASE_NETWORK=$(echo "$NETWORK" | tr '[:lower:]' '[:upper:]')
+
 get_execution_endpoint() {
-    case $_DAPPNODE_GLOBAL_EXECUTION_CLIENT_PRATER in
-    "goerli-geth.dnp.dappnode.eth")
-        echo "http://goerli-geth.dappnode:8545"
+    case $_DAPPNODE_GLOBAL_EXECUTION_CLIENT_${UPPER_CASE_NETWORK} in
+    "${NETWORK}-geth.dnp.dappnode.eth")
+        echo "http://${NETWORK}-geth.dappnode:8545"
         ;;
-    "goerli-nethermind.dnp.dappnode.eth")
-        echo "http://goerli-nethermind.dappnode:8545"
+    "${NETWORK}-nethermind.dnp.dappnode.eth")
+        echo "http://${NETWORK}-nethermind.dappnode:8545"
         ;;
-    "goerli-besu.dnp.dappnode.eth")
-        echo "http://goerli-besu.dappnode:8545"
+    "${NETWORK}-besu.dnp.dappnode.eth")
+        echo "http://${NETWORK}-besu.dappnode:8545"
         ;;
-    "goerli-erigon.dnp.dappnode.eth")
-        echo "http://goerli-erigon.dappnode:8545"
+    "${NETWORK}-erigon.dnp.dappnode.eth")
+        echo "http://${NETWORK}-erigon.dappnode:8545"
         ;;
     *)
-        echo "$_DAPPNODE_GLOBAL_EXECUTION_CLIENT_PRATER"
+        echo "$_DAPPNODE_GLOBAL_EXECUTION_CLIENT_${UPPER_CASE_NETWORK}"
         ;;
     esac
 }
 
 get_beacon_node_endpoint() {
-    case "$_DAPPNODE_GLOBAL_CONSENSUS_CLIENT_PRATER" in
-    "prysm-prater.dnp.dappnode.eth")
-        echo "http://beacon-chain.prysm-prater.dappnode:3500"
+    case "$_DAPPNODE_GLOBAL_CONSENSUS_CLIENT_${UPPER_CASE_NETWORK}" in
+    "prysm-${NETWORK}.dnp.dappnode.eth")
+        echo "http://beacon-chain.prysm-${NETWORK}.dappnode:3500"
         ;;
-    "teku-prater.dnp.dappnode.eth")
-        echo "http://beacon-chain.teku-prater.dappnode:3500"
+    "teku-${NETWORK}.dnp.dappnode.eth")
+        echo "http://beacon-chain.teku-${NETWORK}.dappnode:3500"
         ;;
-    "lighthouse-prater.dnp.dappnode.eth")
-        echo "http://beacon-chain.lighthouse-prater.dappnode:3500"
+    "lighthouse-${NETWORK}.dnp.dappnode.eth")
+        echo "http://beacon-chain.lighthouse-${NETWORK}.dappnode:3500"
         ;;
-    "nimbus-prater.dnp.dappnode.eth")
-        echo "http://beacon-validator.nimbus-prater.dappnode:4500"
+    "nimbus-${NETWORK}.dnp.dappnode.eth")
+        echo "http://beacon-validator.nimbus-${NETWORK}.dappnode:4500"
         ;;
-    "lodestar-prater.dnp.dappnode.eth")
-        echo "http://beacon-chain.lodestar-prater.dappnode:3500"
+    "lodestar-${NETWORK}.dnp.dappnode.eth")
+        echo "http://beacon-chain.lodestar-${NETWORK}.dappnode:3500"
         ;;
     *)
-        echo "$_DAPPNODE_GLOBAL_CONSENSUS_CLIENT_PRATER"
+        echo "$_DAPPNODE_GLOBAL_CONSENSUS_CLIENT_${UPPER_CASE_NETWORK}"
         ;;
     esac
 }
@@ -51,7 +54,7 @@ echo "$KEYSTORES_PASSWORD" >/app/data/keystores/password.txt
 # Write hot wallet password to file
 echo "$OPERATOR_WALLET_PASSWORD" >/app/data/operator/password.txt
 
-exec python3 /app/src/main.py start --network goerli \
+exec python3 /app/src/main.py start --network ${NETWORK} \
     --deposit-data-file /app/data/deposit_data.json \
     --keystores-dir /app/data/keystores \
     --keystores-password-file /app/data/keystores/password.txt \
